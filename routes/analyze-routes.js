79b709b9038cf8449946ac471179af0b3871c4ae
hudanadeem <hudanadeem.02@gmail.com>
2025-03-23 17:35:06 -0400

    updated env file to show nothing and cleaned code

--- routes/analyze-routes.js
+++ routes/analyze-routes.js
@@ -4,9 +4,7 @@ import * as analyzeController from "../controllers/analyze-controller.js";
 import { authenticateUser } from "../middleware/auth-Middleware.js";
 
 const analyzeRouter = express.Router();
-const upload = multer({ dest: "uploads/" }); // Temporary upload location
-
-// Route for image analysis
+const upload = multer({ dest: "uploads/" }); 
 analyzeRouter.post("/", authenticateUser ,upload.single("image"), analyzeController.analyzeImage);
 
 export default analyzeRouter; 
